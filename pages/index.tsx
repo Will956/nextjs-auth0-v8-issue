19a97ed00dce8839e18d8d9ec828e8c099181fa8
Will956 <will.dupl95@gmail.com>
2022-12-29 11:27:52 +0100

    Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import { getAccessToken } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
+import { fetcherClient, fetcherServer } from "./utils/fetch";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const getServersideProps: GetServerSideProps = async (ctx) => {
-  const accessToken = await getAccessToken(ctx.req, ctx.res);
-  console.log("accessToken", accessToken);
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const data = await fetcherServer(ctx, "/api/hello");
+  console.log("data", data);
 
   return {
     props: {
@@ -19,6 +17,14 @@ export const getServersideProps: GetServerSideProps = async (ctx) => {
 };
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetcherClient("/api/hello");
+      console.log("data", data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Head>
@@ -79,10 +85,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2>
               Docs <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p>
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
           </a>
@@ -93,10 +99,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2>
               Learn <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
           </a>
@@ -107,10 +113,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2>
               Templates <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p>
               Discover and deploy boilerplate example Next.js&nbsp;projects.
             </p>
           </a>
@@ -121,10 +127,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2>
               Deploy <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p>
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
---
+++ pages/utils/fetch.ts
@@ -0,0 +1,18 @@
+import { getAccessToken } from "@auth0/nextjs-auth0";
+
+const baseUrl = "http://localhost:3000";
+
+export const fetcherClient = async (url: string) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
+export const fetcherServer = async (ctx: any, url: string) => {
+  const accessToken = await getAccessToken(ctx.req, ctx.res);
+  const res = await fetch(`${baseUrl}${url}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return res.json();
+};
